53edc224160fbac7a33c7c5553d3e6974ba08190
Joris Goosen <Joris@JorisGoosen.nl>
2022-05-18 01:55:47 +0200

    use a trick for envvars in .desktop
    from https://stackoverflow.com/questions/8980464/how-do-i-access-an-environment-variable-in-a-desktop-files-exec-line/8980518#8980518
    maybe this works

--- Tools/CMake/Install.cmake
+++ Tools/CMake/Install.cmake
@@ -164,11 +164,14 @@ if(LINUX)
   install(DIRECTORY ${MODULES_BINARY_PATH}/
           DESTINATION ${JASP_INSTALL_MODULEDIR})
 
-  install(DIRECTORY ${MODULES_RENV_ROOT_PATH}/
-          DESTINATION ${JASP_INSTALL_PREFIX}/lib64/renv-root)
+  # we do not need renv-root in an install
+  #install(DIRECTORY ${MODULES_RENV_ROOT_PATH}/
+  #        DESTINATION ${JASP_INSTALL_PREFIX}/lib64/renv-root)
 
+if(NOT FLATPAK_USED) #because flatpak already puts renv-cache in /app/lib64 anyway
   install(DIRECTORY ${MODULES_RENV_CACHE_PATH}/
           DESTINATION ${JASP_INSTALL_PREFIX}/lib64/renv-cache)
+endif()
 
   # Flatpak Misc.
 
