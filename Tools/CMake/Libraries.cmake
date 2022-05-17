451784bcc164203c359c6d181d73e98a35071686
Joris Goosen <Joris@JorisGoosen.nl>
2022-05-17 18:52:30 +0200

    put webenginequick special cases back

--- Tools/CMake/Libraries.cmake
+++ Tools/CMake/Libraries.cmake
@@ -139,15 +139,22 @@ else()
                QuickControls2Impl
                QmlWorkerScript
                QuickWidgets)
-	
+
+  find_package(
+    Qt6WebEngineQuick
+    REQUIRED
+    PATHS
+    "/app/lib/$ENV{FLATPAK_ARCH}-linux-gnu/cmake/Qt6WebEngineQuick/"
+       ${Qt6WebEngineQuick_DIR}
+    NO_DEFAULT_PATH)
+
   find_package(
     Qt6Core5Compat
     REQUIRED
     PATHS
-	"/app/lib/$ENV{FLATPAK_ARCH}-linux-gnu/cmake/Qt6Core5Compat/"
-	${Qt6Core5Compat_DIR}
+	  "/app/lib/$ENV{FLATPAK_ARCH}-linux-gnu/cmake/Qt6Core5Compat/"
+	  ${Qt6Core5Compat_DIR}
     NO_DEFAULT_PATH)
-
 
 endif()
 
