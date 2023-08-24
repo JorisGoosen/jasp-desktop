3ca0d8e9bf1ceaaa4ec10f756952c98b627bbec9
vandenman <donvdbergh@hotmail.com>
2023-08-24 21:53:45 +0200

    windows fixes, stuff compiles but weird runtime error

--- Tools/CMake/R.cmake
+++ Tools/CMake/R.cmake
@@ -596,6 +596,7 @@ elseif(WIN32)
   set(R_LIBRARY_PATH "${R_HOME_PATH}/library")
   set(R_OPT_PATH "${R_HOME_PATH}/opt")
   set(R_EXECUTABLE "${R_HOME_PATH}/bin/R")
+  set(RSCRIPT_EXECUTABLE "${R_HOME_PATH}/bin/Rscript")
   set(R_INCLUDE_PATH "${R_HOME_PATH}/include")
   set(RCPP_PATH "${R_LIBRARY_PATH}/Rcpp")
   set(RINSIDE_PATH "${R_LIBRARY_PATH}/RInside")
@@ -610,6 +611,7 @@ elseif(WIN32)
   cmake_print_variables(R_LIBRARY_PATH)
   cmake_print_variables(R_OPT_PATH)
   cmake_print_variables(R_EXECUTABLE)
+  cmake_print_variables(RSCRIPT_EXECUTABLE)
 
   cmake_print_variables(RCPP_PATH)
   cmake_print_variables(RINSIDE_PATH)
@@ -685,50 +687,51 @@ elseif(WIN32)
 
   endif()
 
+
+  # NOTE: identical to the code under elseif(LINUX)
+  set(RENV_LIBRARY                "${CMAKE_BINARY_DIR}/_cache/R/renv_library")
+  set(R_CPP_INCLUDES_LIBRARY      "${CMAKE_BINARY_DIR}/R/R_cpp_includes_library")
+#  set(R_CPP_INCLUDES_LIBRARY      "${CMAKE_BINARY_DIR}/R/R_cpp_includes_library")
+  set(JASPMODULEINSTALLER_LIBRARY "${CMAKE_BINARY_DIR}/R/jaspModuleInstaller_library")
+  SET(RENV_SANDBOX                "${CMAKE_BINARY_DIR}/_cache/R/renv_sandbox")
+  file(MAKE_DIRECTORY ${RENV_SANDBOX})
+  # TODO: it would be nice to ship the sandbox so it can be used to install dynamic modules
+  # also, the sandbox paths would probably need to be adjusted on windows
+
+  message(STATUS "Setting up renv, Rcpp, RInside, and jaspModuleInstaller")
+  message(STATUS "RENV_LIBRARY           = ${RENV_LIBRARY}")
+  message(STATUS "R_CPP_INCLUDES_LIBRARY = ${R_CPP_INCLUDES_LIBRARY}")
+
+  configure_file(${PROJECT_SOURCE_DIR}/Tools/setup_renv_rcpp_rinside_jaspModuleInstaller.R.in
+                 ${SCRIPT_DIRECTORY}/setup_renv_rcpp_rinside_jaspModuleInstaller.R @ONLY)
+
+  execute_process(
+    COMMAND_ECHO STDOUT
+    #ERROR_QUIET OUTPUT_QUIET
+    WORKING_DIRECTORY ${R_HOME_PATH}
+    COMMAND ${R_EXECUTABLE} --slave --no-restore --no-save --file=${SCRIPT_DIRECTORY}/setup_renv_rcpp_rinside_jaspModuleInstaller.R)
+
+  set(FIND_PACKAGE_PATH_SCRIPT		${PROJECT_SOURCE_DIR}/Tools/find_package_path.R)
+  macro(find_package_path out libPath packageName)
+    execute_process(COMMAND ${RSCRIPT_EXECUTABLE} ${FIND_PACKAGE_PATH_SCRIPT} ${libPath} ${packageName} OUTPUT_VARIABLE ${out})
+  endmacro()
+  find_package_path(RCPP_PATH       ${R_CPP_INCLUDES_LIBRARY} "Rcpp")
+  find_package_path(RINSIDE_PATH    ${R_CPP_INCLUDES_LIBRARY} "RInside")
+
+  set(RENV_PATH "${RENV_LIBRARY}/renv")
+
+  message(STATUS "RENV_PATH              = ${RENV_PATH}")
+  message(STATUS "RCPP_PATH              = ${RCPP_PATH}")
+  message(STATUS "RINSIDE_PATH           = ${RINSIDE_PATH}")
+
   if(NOT EXISTS ${RENV_PATH})
-    message(STATUS "renv is not installed!")
-    message(CHECK_START "Installing 'renv'")
-
-    configure_file(${MODULES_SOURCE_PATH}/install-renv.R.in
-		           ${SCRIPT_DIRECTORY}/install-renv.R @ONLY)
-
-    execute_process(
-      COMMAND_ECHO STDOUT
-      #ERROR_QUIET OUTPUT_QUIET
-      WORKING_DIRECTORY ${R_HOME_PATH}
-      COMMAND ${R_EXECUTABLE} --slave --no-restore --no-save
-	          --file=${SCRIPT_DIRECTORY}/install-renv.R)
-
-    if(NOT EXISTS ${R_LIBRARY_PATH}/renv)
-      message(CHECK_FAIL "unsuccessful.")
       message(FATAL_ERROR "'renv' installation has failed!")
-    endif()
-
-    message(CHECK_PASS "successful.")
   endif()
-
+  if(NOT EXISTS ${RCPP_PATH})
+      message(FATAL_ERROR "'Rcpp' installation has failed!")
+  endif()
   if(NOT EXISTS ${RINSIDE_PATH})
-    message(STATUS "RInside is not installed!")
-
-    message(CHECK_START "Installing the 'RInside' and 'Rcpp'")
-
-    configure_file(${MODULES_SOURCE_PATH}/install-RInside.R.in
-		           ${SCRIPT_DIRECTORY}/install-RInside.R @ONLY)
-
-    execute_process(
-      COMMAND_ECHO STDOUT
-      #ERROR_QUIET OUTPUT_QUIET
-      WORKING_DIRECTORY ${R_BIN_PATH}
-      COMMAND ${R_EXECUTABLE} --slave --no-restore --no-save
-	          --file=${SCRIPT_DIRECTORY}/install-RInside.R)
-
-    if(NOT EXISTS ${R_LIBRARY_PATH}/RInside)
-      message(CHECK_FAIL "unsuccessful.")
       message(FATAL_ERROR "'RInside' installation has failed!")
-    endif()
-
-    message(CHECK_PASS "successful.")
-
   endif()
 
 elseif(LINUX)
