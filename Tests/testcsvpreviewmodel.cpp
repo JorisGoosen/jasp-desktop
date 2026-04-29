68b0af8d39969b3fa4dd60212fe1e6791bd1367d
Virtuoos Automatisch <virtuoos@jorisgoosen.nl>
2026-04-29 09:03:31 +0200

    test the previewmodel?

---
+++ Tests/testcsvpreviewmodel.cpp
@@ -0,0 +1,28 @@
+#include "testcsvpreviewmodel.h"
+#include <CsvPreviewModel>
+
+void TestCsvPreviewModel::testCsvParsing()
+{
+    CsvPreviewModel model;
+    
+    QString rawData = "Col1,Col2,Col3\nVal1,Val2,Val3\nVal4,Val5,Val6";
+    model.preparePreview(rawData.toStdString().c_str(), ',');
+
+    QCOMPARE(model.rowCount(), 3);
+    QCOMPARE(model.columnCount(), 3);
+
+    // Check first row (header)
+    QCOMPARE(model.data(model.index(0, 0), Qt::DisplayRole).toString(), QString("Col1"));
+    QCOMPARE(model.data(model.index(0, 1), Qt::DisplayRole).toString(), QString("Col2"));
+    QCOMPARE(model.data(model.index(0, 2), Qt::DisplayRole).toString(), QString("Col3"));
+
+    // Check second row
+    QCOMPARE(model.data(model.index(1, 0), Qt::DisplayRole).toString(), QString("Val1"));
+    QCOMPARE(model.data(model.index(1, 1), Qt::DisplayRole).toString(), QString("Val2"));
+    QCOMPARE(model.data(model.index(1, 2), Qt::DisplayRole).toString(), QString("Val3"));
+
+    // Check third row
+    QCOMPARE(model.data(model.index(2, 0), Qt::DisplayRole).toString(), QString("Val4"));
+    QCOMPARE(model.data(model.index(2, 1), Qt::DisplayRole).toString(), QString("Val5"));
+    QCOMPARE(model.data(model.index(2, 2), Qt::DisplayRole).toString(), QString("Val6"));
+}
