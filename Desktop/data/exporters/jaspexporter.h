81aac816b10c808298e47500773e5e5ca45dfd0f
Virtuoos Automatisch <virtuoos@jorisgoosen.nl>
2026-05-06 13:57:33 +0200

    Fix licenses

--- Desktop/data/exporters/jaspexporter.h
+++ Desktop/data/exporters/jaspexporter.h
@@ -1,7 +1,7 @@
 //
 // Copyright (C) 2018-2026 University of Amsterdam
 //
-// This program is free to redistribute and/or modify
+// This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
@@ -9,9 +9,11 @@
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
+// GNU Affero General Public License for more details.
 //
-// If not, see <http://www.gnu.org/licenses/>.
+// You should have received a copy of the GNU Affero General Public
+// License along with this program.  If not, see
+// <http://www.gnu.org/licenses/>.
 //
 
 #ifndef JASPEXPORTER_H
